645fa7a6eccb987c85b66654ba004f07ef5f4b9d
Alok Chauhan <alokc0365@gmail.com>
2024-09-19 22:10:20 +0530

    useState_mini_project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import './App.css';
+// import Button from './Button';
+// function App() {
+//   return (
+//     <div className="App">
+//           <Button></Button>
+//     </div>
+//   );
+// }
+
+
+// export default App;
+import React from 'react'
+import './index.css'
+
+import { useState } from 'react';
+
+
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  
+
+const [step,setStep] = useState(1);
+
+function prevHandler(){
+  if(step>1){
+    setStep(step-1);
+  }
+}
+function nextHandler(){
+  if(step<3){
+    setStep(step+1);
+  } 
 }
 
-export default App;
+  return (
+    <div className='steps'>
+      <div className='numbers'>
+          <div className={step>=1 ? "active" : ""}>1</div>
+          <div className={step>=2 ? "active" : ""}>2</div>
+          <div className={step>=3 ? "active" : ""}>3</div>
+      </div>
+       <p className='message'>Step {step}: {messages[step-1] }
+       </p>
+
+       <div className='buttons'>
+<button style={{backgroundColor:"#7950f2",color:"#fff"}} onClick={prevHandler}>Previous </button>
+<button style={{backgroundColor:"#7950f2",color:"#fff"}} onClick={nextHandler}>next </button>
+
+       </div>
+    </div>
+  )
+}
+
